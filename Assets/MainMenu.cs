ff5d4ad9ed9a70354b3492020c7445d35a1824d1
Incorrect-Username <143725723+Incorrect-Username@users.noreply.github.com>
2024-05-23 19:26:43 +0200

    Commit 2. MenuDone
    Menu Done, Buttons Working. Data Persistence to be implemented

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void ExitGame()
+    {
+        Application.Quit(); // original code to quit Unity player
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
